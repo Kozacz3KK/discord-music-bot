557381f7539492ee932649d2a252f8e0009f322b
kacpe <kacper.kozaczynski@gmail.com>
2025-07-22 20:46:13 +0200

    botszef

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 import os
-
-token = os.getenv("DISCORD_TOKEN")
-print("Token:", token)
-
-if not token:
-    print("Brak tokena! Sprawdź .env lub zmienne środowiskowe.")
-    exit()
-
-import os
 import discord
 from discord.ext import commands
 import youtube_dl
